28784b681bbfa8159ecb36b3c6dfa1bc8f603d9c
holden1337 <ruegger.2@osu.edu>
2021-09-08 13:01:05 -0400

    updated to fit pep8 style guidelines

--- wallchanger.py
+++ wallchanger.py
@@ -14,7 +14,7 @@ import os, sys
 def update_scrollregion(event): #provide scrolling
     photoCanvas.configure(scrollregion=photoCanvas.bbox("all"))
 
-#make sure 2 arguments are included
+# make sure 2 arguments are included
 while True:
     try:
         i3_config = sys.argv[1]
@@ -38,8 +38,8 @@ def callback(num):     #function that changes i3 config
     config.close()  #save i3 config
     os.system('i3-msg restart') #restart i3
 
-imagearray = [] #initialize array of images
-filenames = []  #initialize array of file names
+imagearray = [] # initialize array of images
+filenames = []  # initialize array of file names
 wall_list = os.listdir(wall_folder)
 num_walls = len(wall_list)
 for filename in os.listdir(wall_folder):
@@ -51,26 +51,26 @@ photoFrame = Frame(root, width=1900, height=500, bg="#232629")
 photoFrame.grid()
 photoFrame.rowconfigure(0, weight=1)
 photoFrame.columnconfigure(0, weight=1)
-photoCanvas = Canvas(photoFrame, bg="#232629",width=1900,height=1000)
+photoCanvas = Canvas(photoFrame, bg="#232629", width=1900, height=1000)
 photoCanvas.grid(row=0, column=0, sticky="nsew")
-canvasFrame = Frame(photoCanvas, bg="#EBEBEB",width=1900,height = 1000)
-photoCanvas.create_window(0, 0, window=canvasFrame, anchor='nw',width=1850,height=200*floor((num_walls/9))+200)
+canvasFrame = Frame(photoCanvas, bg="#EBEBEB", width=1900, height = 1000)
+photoCanvas.create_window(0, 0, window=canvasFrame, anchor='nw', width=1850, height=200*floor((num_walls/9))+200)
 
-#create images out of the wallpapers so they can be used on buttons
-#this part is slow which is why the script takes some time to start
-#not sure how to make it faster
+# create images out of the wallpapers so they can be used on buttons
+# this part is slow which is why the script takes some time to start
+# not sure how to make it faster
 for file in os.listdir(wall_folder):
     picture = Image.open(os.path.join(wall_folder,file))
-    picture = picture.resize((200,200),Image.ANTIALIAS)
+    picture = picture.resize((200, 200), Image.ANTIALIAS)
     photo=ImageTk.PhotoImage(picture)
     imagearray.append(photo)
 
-#create grid of bttton with pictures from wallpaper folder
-#when clicked will call callback() function
+# create grid of bttton with pictures from wallpaper folder
+# when clicked will call callback() function
 buttons = []
 count = 0
-for j in range(0,num_walls):
-    buttons.append(Button(canvasFrame,image=imagearray[j],command=(lambda j=j:callback(j)),height=200,width=200,highlightbackground = '#232629',compound=LEFT,borderwidth=0))
+for j in range(0, num_walls):
+    buttons.append(Button(canvasFrame, image=imagearray[j],command=(lambda j=j:callback(j)), height=200, width=200, highlightbackground = '#232629', compound=LEFT, borderwidth=0))
 
     if j%9 == 0:
         count +=1
